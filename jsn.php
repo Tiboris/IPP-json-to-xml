ba1cde7be4224b7e931f1203d370f4e63d406129
Tiboris <xdudla00@vutbr.cz>
2016-03-07 10:26:49 +0100

    help edited

--- jsn.php
+++ jsn.php
@@ -4,48 +4,52 @@
     {
         $help=
         "
-    \t--input=filename 
-    \t\t(UTF-8) in json\n\n
-    \t--outpu=filename 
-    \t\t(UTF-8) in XML\n\n
-    \t-h=subst    
-    \t\tve jméně elementu odvozeném z dvojice jméno-hodnota nahraďte každý nepovolený
-    \t\tznak ve jméně XML značky řetězcem subst. Implicitně (i při nezadaném parametru -h) uvažujte
-    \t\tnahrazování znakem pomlčka (-). Vznikne-li po nahrazení invalidní jméno XML elementu,
-    \t\tskončete s chybou a návratovým kódem 51\n\n
-    \t-n
-    \t\tnegenerovat XML hlavičku 1 na výstup skriptu (vhodné například v případě kombinování více výsledků)\n\n
-    \t-r=root-element
-    \t\tjméno párového kořenového elementu obalujícího výsledek. Pokud nebude
-    \t\tzadán, tak se výsledek neobaluje kořenovým elementem, ač to potenciálně porušuje validitu
-    \t\tXML (skript neskončí s chybou). Zadání řetězce root-element vedoucího na nevalidní XML
-    \t\tznačku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
-    \t--array-name=array-element  
-    \t\ttento parametr umožní přejmenovat element obalující pole
-    \t\tz implicitní hodnoty array na array-element. Zadání řetězce array-element vedoucího na
-    \t\tnevalidní XML značku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
-    \t--item-name=item-element    
-    \t\tanalogicky, tímto parametrem lze změnit jméno elementu pro
-    \t\tprvky pole (implicitní hodnota je item). Zadání řetězce item-element vedoucího na nevalidní
-    \t\tXML značku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
-    \t-s  
-    \t\thodnoty (v dvojici i v poli) typu string budou transformovány na textové elementy místo atributů.\n\n
-    \t-i  
-    \t\thodnoty (v dvojici i v poli) typu number budou transformovány na textové elementy místo atributů.\n\n
-    \t-l  
-    \t\thodnoty literálů (true, false, null) budou transformovány na elementy <true/>,<false/> a <null/> místo na atributy\n\n
-    \t-c  
-    \t\taktivuje překlad problematických znaků.\n\n
-    \t-a, --array-size    
-    \t\tu pole bude doplněn atribut size s uvedením počtu prvků v tomto poli\n\n
-    \t--start=n   
-    \t\tinicializace inkrementálního čitače pro indexaci prvků pole na zadané kladné celé 
-    \t\tčíslo n včetně nuly (implicitně n = 1)\n
-    \t\t(nutno kombinovat s parametrem --index-items, jinak chyba s návratovým kódem 1)\n\n
-    \t-t, --index-items   
-    \t\tke každému prvku pole bude přidán atribut index s určením indexu prvku
-    \t\tv tomto poli (číslování začíná od 1, pokud není parametrem --start určeno jinak).";
-        
+        --input=filename 
+        \t(UTF-8) in json\n
+        --outpu=filename 
+        \t(UTF-8) in XML\n
+        -h=subst    
+        \tve jméně elementu odvozeném z dvojice jméno-hodnota nahraďte každý 
+        \tnepovolený znak ve jméně XML značky řetězcem subst. Implicitně 
+        \t(i při nezadaném parametru -h) uvažujte nahrazování znakem pomlčka (-)
+        \tVznikne-li po nahrazení invalidní jméno XML elementu, skončete s 
+        \tchybou a návratovým kódem 51\n
+        -n
+        \tnegenerovat XML hlavičku 1 na výstup skriptu (vhodné například v 
+        \tpřípadě kombinování více výsledků)\n
+        -r=root-element
+        \tjméno párového kořenového elementu obalujícího výsledek. Pokud nebude
+        \tzadán, tak se výsledek neobaluje kořenovým elementem, ač to 
+        \tpotenciálně porušuje validitu XML (skript neskončí s chybou). Zadání 
+        \třetězce root-element vedoucího na nevalidní XML značku ukončí skript s
+        \tchybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n
+        --array-name=array-element  
+        \ttento parametr umožní přejmenovat element obalující pole
+        \tz implicitní hodnoty array na array-element. Zadání řetězce 
+        \tarray-element vedoucího na nevalidní XML značku ukončí skript s chybou
+        \ta návratovým kódem 50 (nevalidní znaky nenahrazujte).\n
+        --item-name=item-element    
+        \tanalogicky, tímto parametrem lze změnit jméno elementu pro prvky pole 
+        \t(implicitní hodnota je item). Zadání řetězce item-element vedoucího 
+        \tna nevalidní XML značku ukončí skript s chybou a návratovým kódem 50.\n
+        -s  
+        \thodnoty (v dvojici i v poli) typu string budou transformovány 
+        \tna textové elementy místo atributů.\n
+        -i  
+        \thodnoty (v dvojici i v poli) typu number budou transformovány 
+        \tna textové elementy místo atributů.\n
+        -l  
+        \tvalues of literals (true, false, null) will be transformed into 
+        \t<true/>, <false/>, <null/> instead of attributes\n
+        -c  
+        \ttranslation of problematic characters\n
+        -a, --array-size    
+        \tattribute size will be added to an array elements\n
+        -t, --index-items   
+        \tto each element of array will be added atribute index
+        \tstarts from '1' unless parameter --start is set.\n
+        --start=n   
+        \tinitialization of counter for option -t/ --index-items\n";
         echo $help;
         exit(0);
     }
