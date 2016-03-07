7b3c25ad14a900cbceb4143d0af9a46dcb83095e
Tiboris <xdudla00@vutbr.cz>
2016-03-07 09:02:38 +0100

    added -t and fixed invalid combination of -t or --index-items

--- jsn.php
+++ jsn.php
@@ -3,48 +3,48 @@
     function help()
     {
         $help=
-        "\t--input=filename\n 
-            \t\t(UTF-8) in json\n\n
-        \t--outpu=filename 
-            \t\t(UTF-8) in XML\n\n
-        \t-h=subst    
-            \t\tve jméně elementu odvozeném z dvojice jméno-hodnota nahraďte každý nepovolený\n
-            \t\tznak ve jméně XML značky řetězcem subst. Implicitně (i při nezadaném parametru -h) uvažu-\n
-            \t\tjte nahrazování znakem pomlčka (-). Vznikne-li po nahrazení invalidní jméno XML elementu,\n
-            \t\tskončete s chybou a návratovým kódem 51\n\n
-        \t-n
-            \t\tnegenerovat XML hlavičku 1 na výstup skriptu (vhodné například v případě kombinování více výsledků)\n\n
-        \t-r=root-element\n
-            \t\tjméno párového kořenového elementu obalujícího výsledek. Pokud nebude\n
-            \t\tzadán, tak se výsledek neobaluje kořenovým elementem, ač to potenciálně porušuje validitu\n
-            \t\tXML (skript neskončí s chybou). Zadání řetězce root-element vedoucího na nevalidní XML\n
-            \t\tznačku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
-        \t--array-name=array-element \n 
-            \t\ttento parametr umožní přejmenovat element obalující pole\n
-            \t\tz implicitní hodnoty array na array-element. Zadání řetězce array-element vedoucího na\n
-            \t\tnevalidní XML značku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky ne-\n
-            \t\tnahrazujte).\n\n
-        \t--item-name=item-element    \n
-            \t\tanalogicky, tímto parametrem lze změnit jméno elementu pro\n
-            \t\tprvky pole (implicitní hodnota je item). Zadání řetězce item-element vedoucího na nevalidní\n
-            \t\tXML značku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
-        \t-s  \n
-            \t\thodnoty (v dvojici i v poli) typu string budou transformovány na textové elementy místo atributů.\n\n
-        \t-i  \n
-            \t\thodnoty (v dvojici i v poli) typu number budou transformovány na textové elementy místo atributů.\n\n
-        \t-l  \n
-            \t\thodnoty literálů (true, false, null) budou transformovány na elementy <true/>,<false/> a <null/> místo na atributy\n\n
-        \t-c  \n
-            \t\taktivuje překlad problematických znaků.\n\n
-        \t-a, --array-size  \n  
-            \t\tu pole bude doplněn atribut size s uvedením počtu prvků v tomto poli\n\n
-        \t--start=n   \n
-            \t\tinicializace inkrementálního čitače pro indexaci prvků pole na zadané kladné celé\n 
-            \t\tčíslo n včetně nuly (implicitně n = 1)\n
-            \t\t(nutno kombinovat s parametrem --index-items, jinak chyba s návratovým kódem 1)\n\n
-        \t-t, --index-items \n  
-            \t\tke každému prvku pole bude přidán atribut index s určením indexu prvku\n
-            \t\tv tomto poli (číslování začíná od 1, pokud není parametrem --start určeno jinak).\n";
+        "
+    \t--input=filename 
+    \t\t(UTF-8) in json\n\n
+    \t--outpu=filename 
+    \t\t(UTF-8) in XML\n\n
+    \t-h=subst    
+    \t\tve jméně elementu odvozeném z dvojice jméno-hodnota nahraďte každý nepovolený
+    \t\tznak ve jméně XML značky řetězcem subst. Implicitně (i při nezadaném parametru -h) uvažujte
+    \t\tnahrazování znakem pomlčka (-). Vznikne-li po nahrazení invalidní jméno XML elementu,
+    \t\tskončete s chybou a návratovým kódem 51\n\n
+    \t-n
+    \t\tnegenerovat XML hlavičku 1 na výstup skriptu (vhodné například v případě kombinování více výsledků)\n\n
+    \t-r=root-element
+    \t\tjméno párového kořenového elementu obalujícího výsledek. Pokud nebude
+    \t\tzadán, tak se výsledek neobaluje kořenovým elementem, ač to potenciálně porušuje validitu
+    \t\tXML (skript neskončí s chybou). Zadání řetězce root-element vedoucího na nevalidní XML
+    \t\tznačku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
+    \t--array-name=array-element  
+    \t\ttento parametr umožní přejmenovat element obalující pole
+    \t\tz implicitní hodnoty array na array-element. Zadání řetězce array-element vedoucího na
+    \t\tnevalidní XML značku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
+    \t--item-name=item-element    
+    \t\tanalogicky, tímto parametrem lze změnit jméno elementu pro
+    \t\tprvky pole (implicitní hodnota je item). Zadání řetězce item-element vedoucího na nevalidní
+    \t\tXML značku ukončí skript s chybou a návratovým kódem 50 (nevalidní znaky nenahrazujte).\n\n
+    \t-s  
+    \t\thodnoty (v dvojici i v poli) typu string budou transformovány na textové elementy místo atributů.\n\n
+    \t-i  
+    \t\thodnoty (v dvojici i v poli) typu number budou transformovány na textové elementy místo atributů.\n\n
+    \t-l  
+    \t\thodnoty literálů (true, false, null) budou transformovány na elementy <true/>,<false/> a <null/> místo na atributy\n\n
+    \t-c  
+    \t\taktivuje překlad problematických znaků.\n\n
+    \t-a, --array-size    
+    \t\tu pole bude doplněn atribut size s uvedením počtu prvků v tomto poli\n\n
+    \t--start=n   
+    \t\tinicializace inkrementálního čitače pro indexaci prvků pole na zadané kladné celé 
+    \t\tčíslo n včetně nuly (implicitně n = 1)\n
+    \t\t(nutno kombinovat s parametrem --index-items, jinak chyba s návratovým kódem 1)\n\n
+    \t-t, --index-items   
+    \t\tke každému prvku pole bude přidán atribut index s určením indexu prvku
+    \t\tv tomto poli (číslování začíná od 1, pokud není parametrem --start určeno jinak).";
         
         echo $help;
         exit(0);
@@ -52,7 +52,7 @@
 
     function err($errcode) 
     {
-        echo "Program error, exit code '" . $errcode . "' type '--help' for more info.\n" ; // stderr
+        echo "Program error, exit code '" . $errcode . "', type '--help' for more info.\n" ; // stderr
         die($errcode);
     }
     /*
@@ -110,9 +110,16 @@
         if ( $args === false ) { 
             return false;
         }
+        if ( isset( $args['t'] ) && isset( $args['index-items'] ) ) {
+            return false;
+        }
         if ( isset( $args['start'] ) ) 
         {
-            if ( ! isset( $args['index-items'] ) || ( ereg("^[0-9]*$", $args['start']) ) === false  ) {
+            if ( ! isset( $args['t'] ) && ! isset( $args['index-items'] ) ) {
+                echo "asdas";
+                return false;
+            }
+            if ( ( ereg("^[0-9]*$", $args['start']) ) === false ) {
                 return false;
             }
             else {
@@ -182,7 +189,7 @@
         foreach ($array as $key => $value)
         {
             $writer->startElement($args['item-name']);
-            if ( isset( $args['index-items'] ) ) {
+            if ( isset( $args['index-items']) || isset( $args['t']) ) {
                 $writer->writeAttribute('index', $index++);  
             }
             if ( is_object( $value) ) {
@@ -214,7 +221,7 @@
     if ( ( $json_input = file_get_contents($json_input_path) ) === false ) {
         err(2);
     }
-    if ( ( $xml_output = fopen($args['output'], 'w') ) === false ) {
+    if ( ( $xml_output = fopen($args['output'], 'w')) === false ) {
         err(3);
     }
     else {
